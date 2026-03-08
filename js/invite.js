f75ff256ee6b5a6f22098c8bfba2d40cfd65b84a
Sea <usrallc@outlook.com>
2026-03-07 19:03:09 -0800

    Invite circles with icons and social share

--- js/invite.js
+++ js/invite.js
@@ -2,67 +2,42 @@
   'use strict';
 
   RT.loadInvite = function () {
-    if (!RT.isLoggedIn()) { RT.showScreen('auth'); RT.showAuthForm('login'); return; }
+    var link = RT.$('invite-link');
+    var ref = RT.refCode || RT.email.split('@')[0];
+    var url = 'https://resonatale.com?ref=' + ref;
+    if (link) link.textContent = url;
 
-    var code = RT.refCode || RT.email.split('@')[0] + Math.random().toString(36).slice(2, 6);
-    var link = 'https://resonatale.com?ref=' + code;
+    var msg = encodeURIComponent('Create AI films narrated in your own voice! Try ResonaTale: ' + url);
+    var encodedUrl = encodeURIComponent(url);
 
-    var linkEl = RT.$('invite-link');
-    if (linkEl) linkEl.textContent = link;
+    var links = {
+      'btn-invite-whatsapp': 'https://api.whatsapp.com/send?text=' + msg,
+      'btn-invite-x': 'https://twitter.com/intent/tweet?text=' + msg,
+      'btn-invite-facebook': 'https://www.facebook.com/sharer/sharer.php?u=' + encodedUrl,
+      'btn-invite-telegram': 'https://t.me/share/url?url=' + encodedUrl + '&text=' + msg,
+      'btn-invite-instagram': 'https://www.instagram.com/',
+      'btn-invite-tiktok': 'https://www.tiktok.com/',
+      'btn-invite-youtube': 'https://www.youtube.com/',
+      'btn-invite-email': 'mailto:?subject=' + encodeURIComponent('Check out ResonaTale') + '&body=' + msg,
+    };
 
-    var copyBtn = RT.$('btn-copy-invite');
-    if (copyBtn) {
-      copyBtn.onclick = function () {
-        if (navigator.clipboard) {
-          navigator.clipboard.writeText(link).then(function () {
-            RT.toast('Link copied!', true);
-          });
-        }
-      };
-    }
-
-    var container = RT.$('invite-share-buttons');
-    if (container) {
-      container.innerHTML = '';
-      var msg = encodeURIComponent('Create AI films starring YOU! Your face, your voice, your movie. Try it: ' + link);
-      var url = encodeURIComponent(link);
-
-      var platforms = [
-        { label: 'WhatsApp', icon: '💬', href: 'https://api.whatsapp.com/send?text=' + msg },
-        { label: 'X', icon: '𝕏', href: 'https://twitter.com/intent/tweet?text=' + msg },
-        { label: 'Facebook', icon: 'f', href: 'https://www.facebook.com/sharer/sharer.php?u=' + url },
-        { label: 'Telegram', icon: '✈', href: 'https://t.me/share/url?url=' + url + '&text=' + msg },
-        { label: 'LinkedIn', icon: 'in', href: 'https://www.linkedin.com/sharing/share-offsite/?url=' + url },
-        { label: 'Reddit', icon: '●', href: 'https://www.reddit.com/submit?url=' + url + '&title=' + msg },
-        { label: 'Email', icon: '✉', href: 'mailto:?subject=' + encodeURIComponent('Check out ResonaTale!') + '&body=' + msg },
-        { label: 'SMS', icon: '✆', href: 'sms:?body=' + msg },
-        { label: 'Copy', icon: '🔗', href: '#' },
-      ];
-
-      platforms.forEach(function (p) {
-        var a = document.createElement('a');
-        a.className = 'share-circle';
-        a.title = p.label;
-        a.target = '_blank';
-        a.rel = 'noopener noreferrer';
-        a.href = p.href;
-        a.textContent = p.icon;
-
-        if (p.label === 'Copy') {
-          a.href = '#';
-          a.addEventListener('click', function (e) {
-            e.preventDefault();
-            if (navigator.clipboard) {
-              navigator.clipboard.writeText(link).then(function () {
-                RT.toast('Link copied!', true);
-              });
-            }
-          });
-        }
-
-        container.appendChild(a);
-      });
-    }
+    Object.keys(links).forEach(function (id) {
+      var el = RT.$(id);
+      if (el) el.href = links[id];
+    });
   };
+
+  var copyBtn = RT.$('btn-copy-invite');
+  if (copyBtn) {
+    copyBtn.addEventListener('click', function () {
+      var ref = RT.refCode || RT.email.split('@')[0];
+      var url = 'https://resonatale.com?ref=' + ref;
+      navigator.clipboard.writeText(url).then(function () {
+        RT.toast('Link copied!', true);
+      }).catch(function () {
+        RT.toast('Copy failed. Select the link manually.');
+      });
+    });
+  }
 
 })();

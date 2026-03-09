1b1b02c3b117a8511afef072412c7637e3e127b6
Sea <usrallc@outlook.com>
2026-03-09 14:44:43 -0700

    Add voice privacy hint

--- js/create.js
+++ js/create.js
@@ -48,7 +48,6 @@
     { value: 2, id: 'standard', label: '3 minutes',  scenes: 18,  credits: 30,  price: '$30' },
     { value: 3, id: 'feature',  label: '5 minutes',  scenes: 35,  credits: 50,  price: '$50' },
     { value: 4, id: 'epic',     label: '10 minutes',  scenes: 60,  credits: 90,  price: '$90' },
-    { value: 5, id: 'cinema',   label: '20 minutes',  scenes: 150, credits: 199, price: '$199' }
   ];
 
   function updateDuration() {
